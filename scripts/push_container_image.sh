da4c32bfd3d1c0287d80df80d729079fc26379ba
Jens Andresen <jandr@DFDS.COM>
2019-03-14 11:36:50 +0100

    change lineendings

--- scripts/push_container_image.sh
+++ scripts/push_container_image.sh
@@ -1,7 +1,7 @@
 IMAGE_NAME=${1}
 BUILD_NUMBER=${2}
 
-aws ecr get-login --no-include-email
+$(aws ecr get-login --no-include-email)
 registry_image_name="579478677147.dkr.ecr.eu-central-1.amazonaws.com/${IMAGE_NAME}:${BUILD_NUMBER}"
 
 docker tag ${IMAGE_NAME}:latest ${registry_image_name}
