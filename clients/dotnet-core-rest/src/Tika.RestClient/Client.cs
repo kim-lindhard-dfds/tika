6d3911512622115faf501375d5366511abc9c57e
Emil H. Clausen <emcla@dfds.com>
2020-02-03 10:22:36 +0100

    Added new features to TikaClient [ch944]

--- clients/dotnet-core-rest/src/Tika.RestClient/Client.cs
+++ clients/dotnet-core-rest/src/Tika.RestClient/Client.cs
@@ -1,4 +1,7 @@
 using System.Net.Http;
+using Tika.RestClient.Features.Acls;
+using Tika.RestClient.Features.ApiKeys;
+using Tika.RestClient.Features.ServiceAccounts;
 using Tika.RestClient.Features.Topics;
 
 namespace Tika.RestClient
@@ -7,11 +10,17 @@ namespace Tika.RestClient
     {
         private HttpClient _httpClient;
         public ITopicsClient Topics { get; }
-        
+        public IServiceAccountsClient ServiceAccounts { get; }
+        public IApiKeysClient ApiKeys { get; }
+        public IAclsClient Acls { get; }
+
         public Client(HttpClient httpClient)
         {
             _httpClient = httpClient;
             Topics = new TopicsClient(httpClient);
+            ServiceAccounts = new ServiceAccountsClient(httpClient);
+            ApiKeys = new ApiKeysClient(httpClient);
+            Acls = new AclsClient(httpClient);
         }
         
         public void Dispose()
--- clients/dotnet-core-rest/src/Tika.RestClient/IRestClient.cs
+++ clients/dotnet-core-rest/src/Tika.RestClient/IRestClient.cs
@@ -1,4 +1,7 @@
 using System;
+using Tika.RestClient.Features.Acls;
+using Tika.RestClient.Features.ApiKeys;
+using Tika.RestClient.Features.ServiceAccounts;
 using Tika.RestClient.Features.Topics;
 
 namespace Tika.RestClient
@@ -6,5 +9,8 @@ namespace Tika.RestClient
     public interface IRestClient : IDisposable
     {
         ITopicsClient Topics { get; }
+        IServiceAccountsClient ServiceAccounts { get; }
+        IApiKeysClient ApiKeys { get; }
+        IAclsClient Acls { get; }
     }
 }
