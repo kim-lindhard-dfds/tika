0f0c9be3f85c51873d6147983529679e6585abcc
Emil H. Clausen <emcla@dfds.com>
2020-02-12 15:37:26 +0100

    Add handling of no configuration provided for RestClient

--- clients/dotnet-core-rest/src/Tika.RestClient/Factories/RestClientFactory.cs
+++ clients/dotnet-core-rest/src/Tika.RestClient/Factories/RestClientFactory.cs
@@ -13,8 +13,31 @@ namespace Tika.RestClient.Factories
 
         public static IRestClient CreateFromConfiguration(HttpClient httpClient, IOptions<ClientOptions> options)
         {
+            Console.WriteLine("xaxa");
+            if (options.Value?.TIKA_API_ENDPOINT == null)
+            {
+                throw new TikaRestClientInvalidConfigurationException("TIKA_API_ENDPOINT");
+            }
             httpClient.BaseAddress = new Uri(options.Value?.TIKA_API_ENDPOINT);
             return new Client(httpClient);
+        }
+    }
+
+    public class TikaRestClientInvalidConfigurationException : Exception
+    {
+        public TikaRestClientInvalidConfigurationException() : base("RestClientFactory was unable to find the necessary configuration to create a RestClient. Please refer to the configuration section of Tika.RestClient's README.")
+        {
+            
+        }
+
+        public TikaRestClientInvalidConfigurationException(string message) : base($"RestClientFactory was unable to find the necessary configuration for '{message}' to create a RestClient. Please refer to the configuration section of Tika.RestClient's README.")
+        {
+            
+        }
+        
+        public TikaRestClientInvalidConfigurationException(string message, Exception inner) : base($"RestClientFactory was unable to find the necessary configuration for '{message}' to create a RestClient. Please refer to the configuration section of Tika.RestClient's README.", inner)
+        {
+            
         }
     }
 }
