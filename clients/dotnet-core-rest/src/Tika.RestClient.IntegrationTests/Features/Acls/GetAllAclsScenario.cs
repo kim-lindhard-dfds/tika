aece687ba7cec8cfbfbbfe90a2502f7c68e41d02
Emil H. Clausen <emcla@dfds.com>
2020-02-12 14:07:49 +0100

    Use .Single instead of .First for ACL scenario interation test

--- clients/dotnet-core-rest/src/Tika.RestClient.IntegrationTests/Features/Acls/GetAllAclsScenario.cs
+++ clients/dotnet-core-rest/src/Tika.RestClient.IntegrationTests/Features/Acls/GetAllAclsScenario.cs
@@ -64,7 +64,7 @@ namespace Tika.RestClient.IntegrationTests.Features.Acls
                 .Where(acl => acl.Operation == _aclCreateDelete.Operation)
                 .Where(acl => acl.Name == _aclCreateDelete.TopicPrefix)
                 .Where(acl => acl.ConsumerGroupPrefix == _aclCreateDelete.ConsumerGroupPrefix)
-                .First(acl => acl.ServiceAccountId == _aclCreateDelete.ServiceAccountId);
+                .Single(acl => acl.ServiceAccountId == _aclCreateDelete.ServiceAccountId);
         }
     }
 }
