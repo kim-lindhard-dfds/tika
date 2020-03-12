49476b9e9daf51c08f9541e77fe9701fd3fd4438
Emil H. Clausen <sequoiia@hummel.yt>
2020-03-12 14:55:33 +0100

    Added script that helps clearing sa, acls and api-keys

---
+++ scripts/clear_sa_creation_flow.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+CLUSTER_ID=$1
+SERVICE_ACCOUNT_ID=$2
+TOPIC_PREFIX=$3
+NOT_DRY_RUN=$4
+
+if [ "$NOT_DRY_RUN" = "true" ]; then
+    echo "DOING IT LIVE"
+    ccloud service-account delete $SERVICE_ACCOUNT_ID
+    ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation WRITE --topic $TOPIC_PREFIX --prefix
+    ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation READ --topic $TOPIC_PREFIX --prefix
+    ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation CREATE --topic $TOPIC_PREFIX --prefix
+    ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation DESCRIBE --topic $TOPIC_PREFIX --prefix
+    ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation DESCRIBE-CONFIGS --topic $TOPIC_PREFIX --prefix
+
+    ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation WRITE --consumer-group $TOPIC_PREFIX --prefix
+    ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation CREATE --consumer-group $TOPIC_PREFIX --prefix
+    ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation READ --consumer-group $TOPIC_PREFIX --prefix
+
+    ccloud kafka acl delete --cluster $CLUSTER_ID --deny --service-account $SERVICE_ACCOUNT_ID --operation alter --cluster-scope
+    ccloud kafka acl delete --cluster $CLUSTER_ID --deny --service-account $SERVICE_ACCOUNT_ID --operation alter-configs --cluster-scope
+    ccloud kafka acl delete --cluster $CLUSTER_ID --deny --service-account $SERVICE_ACCOUNT_ID --operation cluster-action --cluster-scope
+    ccloud kafka acl delete --cluster $CLUSTER_ID --deny --service-account $SERVICE_ACCOUNT_ID --operation create --topic '*' --prefix
+else
+    echo "SERVICE_ACCOUNT_ID=$SERVICE_ACCOUNT_ID"
+    echo "TOPIC_PREFIX=$TOPIC_PREFIX"
+
+    echo "\nThe following commands will be run if 'true' is appended:\n"
+    echo "ccloud service-account delete $SERVICE_ACCOUNT_ID"
+
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation WRITE --topic $TOPIC_PREFIX --prefix"
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation READ --topic $TOPIC_PREFIX --prefix"
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation CREATE --topic $TOPIC_PREFIX --prefix"
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation DESCRIBE --topic $TOPIC_PREFIX --prefix"
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation DESCRIBE-CONFIGS --topic $TOPIC_PREFIX --prefix"
+
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation WRITE --consumer-group $TOPIC_PREFIX --prefix"
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation CREATE --consumer-group $TOPIC_PREFIX --prefix"
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --allow --service-account $SERVICE_ACCOUNT_ID --operation READ --consumer-group $TOPIC_PREFIX --prefix"
+
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --deny --service-account $SERVICE_ACCOUNT_ID --operation alter --cluster-scope"
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --deny --service-account $SERVICE_ACCOUNT_ID --operation alter-configs --cluster-scope"
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --deny --service-account $SERVICE_ACCOUNT_ID --operation cluster-action --cluster-scope"
+    echo "ccloud kafka acl delete --cluster $CLUSTER_ID --deny --service-account $SERVICE_ACCOUNT_ID --operation create --topic '*' --prefix"
+fi
