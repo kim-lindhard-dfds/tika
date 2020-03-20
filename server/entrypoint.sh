338835ca1f6c6ba725e8d512393263f9a3bd2c67
Kim Lindhard <kilin@dfds.com>
2020-03-20 16:18:31 +0100

    correct user runs the cron job

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-chmod +x /app/login.sh
-#/app/login.sh
-
-#crond -f 
-#echo '* * * * * echo hello' | crontab - && crond -f -L /dev/stdout
-
-#echo "SHELL=/bin/bash" >> scheduler
-#echo "BASH_ENV=/container.env" >> scheduler
-#echo "*/30 * * * * /app/login.sh >> /var/log/cron.log 2>&1" >> scheduler
-#touch /var/log/cron.log
-#crontab scheduler
-#cron -f &
-
-#(cd /etc/init.d/ && crond status)
+#chmod +x /app/login.sh
 
 
+crond -b -L /var/log/cron.log "$@" && tail -f /var/log/cron.log & #
 
-tail -f /var/log/cron.log &
 
-#/app/cron-start.sh
 node main.js
