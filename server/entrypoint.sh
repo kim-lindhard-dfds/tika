82c2e2d192ceef66be0e6841b2607f99d0c849bb
Kim Lindhard <kilin@dfds.com>
2020-03-23 10:46:53 +0100

    replace crond with while loop

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/ash
 
-/app/login.sh
-
-# Add /app/login.sh to cron every 30 min & calls the cron demon (vent thou it is started by root) & outputs the cron log file in current users console
-echo '*/30 * * * * /app/login.sh' >> /etc/crontabs/appuser 
-crond -b -L /var/log/cron.log "$@" 
-tail -f /var/log/cron.log &
+while true; do /app/login.sh; sleep 30m; done &
 
 node /app/main.js
