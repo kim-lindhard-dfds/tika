41d0d4e8f09bcfcca6c20ff2225909cfe13d933d
Kim Lindhard <kilin@dfds.com>
2020-04-03 15:11:23 +0200

    ConfigureTopicScenario will cleanup after itself

--- clients/dotnet-core-rest/src/Tika.RestClient.IntegrationTests/Features/Topics/ConfigureTopicScenario.cs
+++ clients/dotnet-core-rest/src/Tika.RestClient.IntegrationTests/Features/Topics/ConfigureTopicScenario.cs
@@ -6,7 +6,7 @@ using Xunit;
 
 namespace Tika.RestClient.IntegrationTests.Features.Topics
 {
-    public class ConfigureTopicScenario
+    public class ConfigureTopicScenario : IDisposable
     {
         private IRestClient _client;
         private string _topicName;
@@ -18,26 +18,25 @@ namespace Tika.RestClient.IntegrationTests.Features.Topics
         [Fact]
         public async Task ConfigureTopicScenarioRecipe()
         {
-                  Given_a_topic_client();
+            Given_a_topic_client();
             await When_creating_a_topic_with_configuration();
             await And_get_description_on_same_topic();
-                  Then_the_configurations_are_the_same();
+            Then_the_configurations_are_the_same();
         }
 
         private void Given_a_topic_client()
         {
             _client = LocalhostRestClient.Create();
         }
-        
+
         private async Task When_creating_a_topic_with_configuration()
         {
             _topicName = "Integration-test-" + Guid.NewGuid();
-            
             var random = new Random();
-            
-            _messageRetentionPeriod = TimeSpan.FromDays(random.Next(1, 11));
+
+            _messageRetentionPeriod = TimeSpan.FromDays(random.Next(1, 7));
             _maxDiskUsageInMb = random.Next(1, 2001);
-            
+
             var topicCreate = TopicCreate.CreateWithRetention(
                 name: _topicName,
                 partitionCount: 1,
@@ -55,15 +54,21 @@ namespace Tika.RestClient.IntegrationTests.Features.Topics
 
         private void Then_the_configurations_are_the_same()
         {
-            var returnedMessageRetentionPeriodInMs = (Int64)_returnedTopicDescription.configurations["retention.ms"];
+            var returnedMessageRetentionPeriodInMs = (Int64) _returnedTopicDescription.configurations["retention.ms"];
             var returnedMessageRetentionPeriod = TimeSpan.FromMilliseconds(returnedMessageRetentionPeriodInMs);
 
             var returnedMaxDiskUsageInBytes =
                 (Int64) _returnedTopicDescription.configurations["retention.bytes"];
             var returnedMaxDiskUsageInMb = returnedMaxDiskUsageInBytes / 1000000;
-            
+
             Assert.Equal(_messageRetentionPeriod, returnedMessageRetentionPeriod);
             Assert.Equal(_maxDiskUsageInMb, returnedMaxDiskUsageInMb);
+        }
+
+        public void Dispose()
+        {
+            var task = _client.Topics.DeleteAsync(_topicName);
+            task.Wait();
         }
     }
 }
