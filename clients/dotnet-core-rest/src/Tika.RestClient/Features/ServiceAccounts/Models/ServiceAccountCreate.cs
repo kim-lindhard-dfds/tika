8fd693ed95065a826b1b63a1c6223d1eb416ceba
Kim Lindhard <kilin@dfds.com>
2020-04-06 16:00:40 +0200

    ServiceAccountCreate will throw if name is longer than 32

--- clients/dotnet-core-rest/src/Tika.RestClient/Features/ServiceAccounts/Models/ServiceAccountCreate.cs
+++ clients/dotnet-core-rest/src/Tika.RestClient/Features/ServiceAccounts/Models/ServiceAccountCreate.cs
@@ -1,8 +1,26 @@
+using Tika.RestClient.Features.ServiceAccounts.Exceptions;
+
 namespace Tika.RestClient.Features.ServiceAccounts.Models
 {
     public class ServiceAccountCreate
     {
-        public string name { get; set; }
+        internal const int MAX_NAME_LENGTH = 32;
+
+        private string _name;
+        public string name
+        {
+            get => _name;
+            set
+            {
+                if (MAX_NAME_LENGTH < value.Length)
+                {
+                    throw new ServiceAccountNameTooLong(value);
+                }
+
+                _name = value;
+            }
+        }
+
         public string description { get; set; }
     }
 }
